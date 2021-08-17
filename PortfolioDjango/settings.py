0bff1a0c2483bed312ef90dcbf4c6a7a63aa69cb
Taber Bin Zameer <tbzameer@gmail.com>
2021-08-18 04:22:28 +0500

    deploy

--- PortfolioDjango/settings.py
+++ PortfolioDjango/settings.py
@@ -83,17 +83,19 @@ WSGI_APPLICATION = 'PortfolioDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Portfolio-Django',
-        'USER': 'postgres',
-        'PASSWORD': 'taber123',
-        'HOST': 'localhost'
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'Portfolio-Django',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'taber123',
+        # 'HOST': 'localhost'
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
