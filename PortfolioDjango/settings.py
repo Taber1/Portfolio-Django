e17b8d136d5d0be9caae01ac9b2eaf04e79ddd3c
Taber Bin Zameer <tbzameer@gmail.com>
2021-09-10 02:51:27 +0500

    Amazon s3

--- PortfolioDjango/settings.py
+++ PortfolioDjango/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+# Application definition+
 
 INSTALLED_APPS = [
     'PortfolioItems.apps.PortfolioitemsConfig',
@@ -163,7 +163,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'portfoliodjango'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
