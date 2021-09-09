f750d26e3e110bde8211d5f07cccfcc32afc9b01
Taber Bin Zameer <tbzameer@gmail.com>
2021-09-10 04:39:39 +0500

    Amazon s3

--- PortfolioDjango/settings.py
+++ PortfolioDjango/settings.py
@@ -168,7 +168,12 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
+AWS_LOCATION = 'static'
+AWS_LOCATION_MEDIA = 'media'
 
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_ADDRESSING_STYLE = "virtual"
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
